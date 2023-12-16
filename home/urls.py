237a34f5c3060b8304c3195d7acf439a46f6fb92
Himanshu Shekhar <hshekhar6008@gmail.com>
2023-12-16 19:24:57 +0530

    commit

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 # home/urls.py
 from django.urls import path
 from .views import home
+from .views import team
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about/team', team, name='team')
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def team(request):
+    return render(request , 'team.html')
